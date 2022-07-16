ee298491c0101953ef975645495086e64c1eb509
GabeSobarzo <91505966+GabeSobarzo@users.noreply.github.com>
2022-07-15 19:44:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -99,5 +99,9 @@ app.get('/account/all', function (req, res) {
 });
 
 var port = 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 app.listen(port);
 console.log('Running on port: ' + port);
