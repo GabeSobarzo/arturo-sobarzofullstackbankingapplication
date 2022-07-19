e88daa88e77980b53d768b78d12c6a9a235fc06d
GabeSobarzo <91505966+GabeSobarzo@users.noreply.github.com>
2022-07-19 11:48:21 -0500

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+const url         = process.env.MONGODB_URI;
 let db            = null;
  
 // connect to mongo
